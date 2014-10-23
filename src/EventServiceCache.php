fea100c8d82edce80b31a40fe0c885d9105e065f
Kristof Coomans <kristof@coomans.eu>
2014-10-23 08:31:28 +0200

    Add Doctrine Cache decorator for event service

---
+++ src/EventServiceCache.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @file
+ */
+
+namespace CultuurNet\UDB3\Doctrine;
+
+
+use CultuurNet\UDB3\EventServiceDecoratorBase;
+use CultuurNet\UDB3\EventServiceInterface;
+use Doctrine\Common\Cache\Cache;
+
+class EventServiceCache extends EventServiceDecoratorBase
+{
+    /**
+     * @var Cache
+     */
+    protected $cacheProvider;
+
+    /**
+     * Cache lifetime, 1 hour by default.
+     *
+     * @var int
+     */
+    protected $lifetime = 3600;
+
+    public function __construct(
+        EventServiceInterface $eventService,
+        Cache $cacheProvider
+    ) {
+        parent::__construct($eventService);
+
+        $this->cacheProvider = $cacheProvider;
+    }
+
+    public function setLifetime($lifetime)
+    {
+        $this->lifetime = $lifetime;
+    }
+
+    public function getEvent($id)
+    {
+        $data = $this->cacheProvider->fetch($id);
+        if (false === $data) {
+            $data = parent::getEvent($id);
+            $this->cacheProvider->save($id, $data, $this->lifetime);
+        }
+    }
+
+
+}
